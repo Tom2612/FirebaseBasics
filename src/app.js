6fb17288241a24dce814a6c204b8b5c0fc017c99
Tom <tomcat-26@hotmail.co.uk>
2022-11-30 18:10:15 +0800

    Trying to build auth in firebase

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js'; 
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js'; 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgESOq4rCvWQaW7CNvUM9z-r_U5Js7ncQ",
+    authDomain: "firebasics-bd259.firebaseapp.com",
+    projectId: "firebasics-bd259",
+    storageBucket: "firebasics-bd259.appspot.com",
+    messagingSenderId: "972637127663",
+    appId: "1:972637127663:web:cc6c52ed02f58ae47cec1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+let email, password;
+
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+emailInput.addEventListener('input', (e) => {
+    email = e.target.value
+})
+passwordInput.addEventListener('input', (e) => {
+    password = e.target.value
+})
+
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener('click', createUserWithEmailAndPassword)
+
+
+createUserWithEmailAndPassword(auth, email=document.querySelector('#email').value, password=document.querySelector('#password').value)
+.then((usercredential) => {
+    const user = usercredential.user;
+    console.log(user)
+})
+.catch(error => {
+    const errCode = error.code;
+    const errMessage = error.message
+    console.log(errCode, errMessage)
+})
+
+console.log(document.querySelector('#email'))
+
+// onAuthStateChanged(auth, user => {
+//     if (user !== null) {
+//         console.log('Logged in!')
+//     } else {
+//         console.log('Not logged in!')
+//     }
+// })
